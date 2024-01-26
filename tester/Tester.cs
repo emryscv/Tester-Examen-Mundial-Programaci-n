f4f2129b284a5cfc12a3ecdcdc0259fd56a83db1
emryscv <emryscv@gmail.com>
2024-01-25 21:34:15 -0500

    first commit

---
+++ tester/Tester.cs
@@ -0,0 +1,20 @@
+namespace MatCom.Tester;
+
+public class Tester : TesterBase<int, int, int, int>
+{
+    public override int InputGenerator(int seed, int arg)
+    {
+        var random = new Random(seed);
+        return random.Next(0, arg);
+    }
+
+    public override bool OutputChecker(int input, int output, int expectedOutput)
+    {
+        return output == expectedOutput;
+    }
+
+    public override int OutputGenerator(int input)
+    {
+        return 2 * input;
+    }
+}
